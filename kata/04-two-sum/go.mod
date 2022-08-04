fa811e0be6df8dd19e08177d9bf34f13f5a27f2c
Oliver Isaac <oisaac@gmail.com>
2022-08-04 13:47:57 -0500

    :zap: solved using binary search from slices pakcage

--- kata/04-two-sum/go.mod
+++ kata/04-two-sum/go.mod
@@ -1,3 +1,5 @@
 module github.com/oliverisaac/code-wars/kata/04-two-sum
 
 go 1.18
+
+require golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
--- kata/04-two-sum/main.go
+++ kata/04-two-sum/main.go
@@ -1,24 +1,28 @@
 package kata
 
-import "sort"
+import (
+	"sort"
+
+	"golang.org/x/exp/slices"
+)
 
 func TwoSum(numbers []int, target int) [2]int {
 	ret := []int{-1, -1}
 
 	// Sort the incoming numbers
-	sort.Ints(numbers)
+	slices.Sort(numbers)
 
 	for firstIndex, firstVal := range numbers {
-		for secondIndex, secondVal := range numbers {
-			// We do not want to use the same index twice
-			if secondIndex == firstIndex {
-				continue
-			}
+		// We want to search for values that are after our current index
+		searchSlice := numbers[firstIndex+1:]
+		searchValue := target - firstVal
 
-			if secondVal+firstVal == target {
-				ret[0] = firstIndex
-				ret[1] = secondIndex
-			}
+		secondIndex, foundValue := slices.BinarySearch(searchSlice, searchValue)
+		if foundValue {
+			ret[0] = firstIndex
+			// The index returned from the searc his relative to our searchSlice's base index
+			ret[1] = firstIndex + 1 + secondIndex
+			break
 		}
 	}
 
