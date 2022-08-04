573072f4f8aa23a2eee76aeec70a4f4cec455d14
Oliver Isaac <oisaac@gmail.com>
2022-08-04 13:49:48 -0500

    :bug: had to implement custom binary search as code-wars did not let me import the slices

--- kata/04-two-sum/main.go
+++ kata/04-two-sum/main.go
@@ -1,23 +1,51 @@
 package kata
 
 import (
+	"math"
 	"sort"
-
-	"golang.org/x/exp/slices"
 )
 
+func binarySearch(haystack []int, needle int) (index int, found bool) {
+	if len(haystack) == 0 {
+		return -1, false
+	}
+
+	if len(haystack) == 1 {
+		return 0, haystack[0] == needle
+	}
+
+	// Middle is the middle value. Array of 2: middle is 1, array of 3, middle is 2
+	middle := int(math.Ceil(float64(len(haystack)) / 2))
+
+	if haystack[middle] == needle {
+		return middle, true
+	}
+
+	if haystack[middle] > needle {
+		resultIndex, isFound := binarySearch(haystack[:middle], needle)
+		return resultIndex, isFound
+	}
+
+	if haystack[middle] < needle {
+		resultIndex, isFound := binarySearch(haystack[middle+1:], needle)
+		return middle + 1 + resultIndex, isFound
+	}
+
+	return -1, false
+}
+
 func TwoSum(numbers []int, target int) [2]int {
-	ret := []int{-1, -1}
+	ret := []int{-2, -2}
 
 	// Sort the incoming numbers
-	slices.Sort(numbers)
+	sort.Ints(numbers)
 
 	for firstIndex, firstVal := range numbers {
 		// We want to search for values that are after our current index
 		searchSlice := numbers[firstIndex+1:]
 		searchValue := target - firstVal
 
-		secondIndex, foundValue := slices.BinarySearch(searchSlice, searchValue)
+		secondIndex, foundValue := binarySearch(searchSlice, searchValue)
 		if foundValue {
 			ret[0] = firstIndex
 			// The index returned from the searc his relative to our searchSlice's base index
